1bcfbec349a075d3f7c321705345977e4ae978e2
MDostalek <dostalek.michal@atngmbh.de>
2023-06-13 13:17:03 +0200

    dev-nbdell-model-controller

--- src/main/java/com/core/Controller.java
+++ src/main/java/com/core/Controller.java
@@ -3,36 +3,52 @@ package com.core;
 import com.gui.GraphPanel;
 import com.model.Constants;
 import com.model.FinanceModel;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
-import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.JOptionPane;
 
+/**
+ * Controller for the MVC.
+ */
 public class Controller implements ActionListener {
     private JTextField searchTermTextField = new JTextField(26);
     private FinanceModel model;
     private FinanceModel modelOverview;
     private GraphPanel graph;
     private JFrame frame;
-    //private DBConnector database;
 
     private String searchColumn = "Month";
     private String searchYear = "";
     private String searchMonth = "";
-    
+
+    /**
+     * Search for a specific string.
+     * @param filterOption search term
+     */
     public void setFilterOption(String filterOption) {
         this.searchColumn = filterOption;
     }
 
+    /**
+     * Sets the year and motnh option.
+     * @param year year
+     * @param month month
+     */
     public void setOverviewDetails(String year, String month) {
         this.searchYear = year;
         this.searchMonth = month;
     }
  
+    /**
+     * Contructor for the MVC controller.
+     * @param model_db db pointer
+     * @param model_sum filtered db pointer (year and month)
+     * @param graph graph panel for refresh
+     * @param frame frame panel for repaint
+     */
     public Controller(FinanceModel model_db, FinanceModel model_sum, GraphPanel graph, JFrame frame) {
         super();
         this.model = model_db;
@@ -41,26 +57,32 @@ public class Controller implements ActionListener {
         this.graph = graph;
     }
 
+    /**
+     * Sets the filter for searching term.
+     * @param searchTermTextField search term
+     */
     public void setFilterTextField(JTextField searchTermTextField) {
         this.searchTermTextField = searchTermTextField;
     }
+    // delete after debuging
     private int i;
+    
     @Override
     public void actionPerformed(ActionEvent e) {      
         String searchTerm = searchTermTextField.getText();
-        if(((JButton)e.getSource()).getActionCommand().equals("Update")){            
+        if (((JButton)e.getSource()).getActionCommand().equals("Update")) {            
             System.out.println("Info od update");
             Object[][] data = ((FinanceModel)modelOverview).getDataForSpecificDate(searchYear, searchMonth);
 
             graph.clearChart();
             modelOverview.setDataVector(data, Constants.TABLE_HEADER);
-            modelOverview.setChartValues(i++);
+            modelOverview.setChartValues(++i);
             graph.updateChart(modelOverview);
             frame.validate();
             frame.repaint();
         }
 
-        if(((JButton)e.getSource()).getActionCommand().equals("Filter")){            
+        if (((JButton)e.getSource()).getActionCommand().equals("Filter")) {            
             System.out.println("Info od filtru");
         
             Object[][] data = ((FinanceModel)model).getDataFromDB(searchColumn, searchTerm);
