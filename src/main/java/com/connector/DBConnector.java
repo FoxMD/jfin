a090f6773c66835ce40583583c78efa30e485ec3
MDostalek <dostalek.michal@atngmbh.de>
2023-06-13 10:31:14 +0200

    dev-nbdell-dbconnectoronly

--- src/main/java/com/connector/DBConnector.java
+++ src/main/java/com/connector/DBConnector.java
@@ -1,13 +1,11 @@
 package com.connector;
 
 import com.secret.Credentials;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import java.util.ArrayList;
 
 public class DBConnector {
@@ -27,54 +25,14 @@ public class DBConnector {
     public Object[][] getQuery(String what, String with) {
         ArrayList<Object[]> Data = new ArrayList<>();
         String REQUEST = "";
-        if("*".equals(with.trim())) {
+        if("*".equals(with.trim())){
             REQUEST = BASE_QUERY;
+        } else {
+            REQUEST = this.QUERY + " " + what + " LIKE \"" + with + "%\"";
         }
-        else {
-            REQUEST = this.QUERY + " " + what + " LIKE \"" + with + "%\""; 
-        }
-        
+
         System.out.println(REQUEST);
-        try 
-        (
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(REQUEST);
-        ) {
-            while(rs.next()) {
-                Data.add(new Object[]{  rs.getString("year"),
-                                        rs.getString("month"),
-                                        rs.getString("type"),
-                                        rs.getFloat("value"),
-                                        rs.getString("currency"),
-                                        rs.getString("description"),
-                                    });
-                System.out.print("Month: " + rs.getString("month"));
-                System.out.print(", Type: " + rs.getString("type"));
-                System.out.print(", Value: " + rs.getFloat("value"));
-                System.out.println(", Currency: " + rs.getString("currency"));
-            }
-
-            rs.close();
-            stmt.close();
-            conn.close();
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        Object[][] dataObject = Data.toArray(new Object[0][0]);
-        return dataObject; 
-    }
-
-    public Object[][] getQueryForYearAndMonth(String year, String month) {
-        ArrayList<Object[]> Data = new ArrayList<>();
-        String REQUEST = "";
-
-        REQUEST = this.QUERY + " month='" + month + "' AND year='" + year + "'"; 
-        
-        System.out.println(REQUEST);
-        try 
+        try
         (
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement();
@@ -97,33 +55,70 @@ public class DBConnector {
             rs.close();
             stmt.close();
             conn.close();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
         Object[][] dataObject = Data.toArray(new Object[0][0]);
-        return dataObject; 
+        return dataObject;
+    }
+
+    public Object[][] getQueryForYearAndMonth(String year, String month) {
+        ArrayList<Object[]> data = new ArrayList<>();
+        String REQUEST = "";
+
+        REQUEST = this.QUERY + " month='" + month + "' AND year='" + year + "'"; 
+        
+        System.out.println(REQUEST);
+        try
+        (
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(REQUEST);
+        ) {
+            while(rs.next()){
+                data.add(new Object[]{  rs.getString("year"),
+                                        rs.getString("month"),
+                                        rs.getString("type"),
+                                        rs.getFloat("value"),
+                                        rs.getString("currency"),
+                                        rs.getString("description"),
+                                    });
+                System.out.print("Month: " + rs.getString("month"));
+                System.out.print(", Type: " + rs.getString("type"));
+                System.out.print(", Value: " + rs.getFloat("value"));
+                System.out.println(", Currency: " + rs.getString("currency"));
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Object[][] dataObject = data.toArray(new Object[0][0]);
+        return dataObject;
     }
 
     public Object[][] getPreciseQuery(String what, String with) {
-        ArrayList<Object[]> Data = new ArrayList<>();
+        ArrayList<Object[]> data = new ArrayList<>();
 
-        Data.add(new Object[]{"January","GROCERY",199.99,"CZK"});
-        Data.add(new Object[]{"February","GROCERY",9.99,"EUR"});
+        data.add(new Object[]{"January", "GROCERY", 199.99, "CZK"});
+        data.add(new Object[]{"February", "GROCERY", 9.99, "EUR"});
 
-        Object retData[][] = Data.toArray(new Object[0][0]);
+        Object retData[][] = data.toArray(new Object[0][0]);
         return retData;
     }
 
     public int testConnection() {
-        try 
+        try
         (
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(BASE_QUERY);
         ) {
-            while(rs.next()) {
+            while(rs.next()){
                 System.out.print("Month: " + rs.getString("month"));
                 System.out.print(", Type: " + rs.getString("type"));
                 System.out.print(", Value: " + rs.getFloat("value"));
@@ -133,8 +128,7 @@ public class DBConnector {
                 //stmt.close();
                 //conn.close();
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             return -1;
         }
