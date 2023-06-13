ef97f2533fe8de060a75a738e366499b21b7c563
FoxMD <ryusei@seznam.cz>
2023-06-13 21:01:58 +0200

    dev-pc-app-start

---
+++ src/main/java/com/core/Start.java
@@ -0,0 +1,38 @@
+package com.core;
+
+import javax.swing.SwingUtilities;
+import com.gui.View;
+
+/**
+ * Start of the program.
+ */
+public final class Start {
+    /**
+     * Constructor of start.
+     */
+    private Start() {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Program started");
+        SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+            try
+            {
+                createAndShowGUI();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        });
+    }
+
+    /**
+     * Create UI and start the app.
+     * @throws Exception For not starting
+     */
+    public static void createAndShowGUI() throws Exception {
+        System.out.println("Create GUI");
+        new View();
+    }
+}
