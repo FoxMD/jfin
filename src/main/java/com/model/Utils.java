ea9b0b52f7e1ff5f4be3e761cb46184f18a8fff4
FoxMD <ryusei@seznam.cz>
2023-07-04 16:29:28 +0200

    Utils docs etc

--- src/main/java/com/model/Utils.java
+++ src/main/java/com/model/Utils.java
@@ -4,12 +4,5 @@ package com.model;
  * Utility class with some sizes.
  */
 public class Utils {
-    public enum Entries {YEAR, MONTH, TYPE, VALUE, CURRENCY, DESC}
-/*
-    static public final int YEAR = 0;
-    static public final int MONTH = 1;
-    static public final int TYPE = 2;
-    static public final int VALUE = 3;
-    static public final int CURRENCY = 4;
-    static public final int DESC = 5;*/
+    public enum Entries { YEAR, MONTH, TYPE, VALUE, CURRENCY, DESC }
 }
